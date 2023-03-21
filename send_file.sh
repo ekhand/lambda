699e547c2000db6790d0bcba6c3773cef24fa53f
ekhand <ehandwerk@flexgen.com>
2023-03-21 11:14:50 -0400

    woche

--- send_file.sh
+++ send_file.sh
@@ -10,7 +10,7 @@ do
 done
 
 ID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-echo "Kaufen Sie jeden Tag vier gute bequeme Pelze!" > build/file.txt
+echo "Kaufen Sie jede Woche vier gute bequeme Pelze!" > build/file.txt
 awslocal --endpoint-url=http://localhost:4566 s3api put-object --bucket $funcname --key $ID --body=build/file.txt
 echo "sent file with key $ID to bucket $funcname"
 
